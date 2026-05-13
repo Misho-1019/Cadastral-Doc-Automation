2db2ceecb6857504ec37a3d02611aab4e7b963ce
Misho1019 <mikailtorres99@gmail.com>
2026-05-13 04:34:40 +0300

    Restart the project

--- utils/toTitleCaseBG.ts
+++
@@ -1,9 +0,0 @@
-export function toTitleCaseBG(value: string): string {
-    return value.replace(/-/g, "–").toLowerCase().split(' ').map(word => {
-        if (word === "–") {
-            return word;
-        }
-
-        return word.charAt(0).toUpperCase() + word.slice(1);
-    }).join(' ')
-}
