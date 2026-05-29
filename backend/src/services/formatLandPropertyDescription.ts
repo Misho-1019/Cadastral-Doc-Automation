1325d4204b947652eb9758def12f300c403fc4c3
Misho1019 <mikailtorres99@gmail.com>
2026-05-29 23:14:50 +0300

    Expand land property description formatter

--- backend/src/services/formatLandPropertyDescription.ts
+++ backend/src/services/formatLandPropertyDescription.ts
@@ -13,6 +13,6 @@ export function formatLandPropertyDescription(data: LandPropertyData): string {
         : 'няма данни за сгради';
 
     return `
-Поземлен имот с идентификатор ${data.identifier}, находящ се в ${data.address}, с площ ${data.area}, с трайно предназначение на територията: ${data.territoryPurpose}, с начин на трайно ползване: ${data.permanentUsage}, при стар идентификатор: ${data.previousIdentifier}, номер по предходен план: ${data.previousPlanNumber}, квартал: ${data.quarter}, парцел: ${data.parcel}, при съседи: ${neighbours}, ведно с построените в имота: ${buildings}.
+Поземлен имот с идентификатор ${data.identifier}, находящ се в ${data.address}, с площ ${data.area}, с трайно предназначение на територията: ${data.territoryPurpose}, с начин на трайно ползване: ${data.permanentUsage}, при предходен идентификатор: ${data.previousIdentifier}, номер по предходен план: ${data.previousPlanNumber}, квартал: ${data.quarter}, парцел: ${data.parcel}, при съседи: ${neighbours}, ведно с построените в имота: ${buildings}.
     `.trim();
 }
