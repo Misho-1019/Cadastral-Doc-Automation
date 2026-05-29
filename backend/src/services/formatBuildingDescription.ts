985cdfbbceca507c248a84c9b85b2ae128c6b248
Misho1019 <mikailtorres99@gmail.com>
2026-05-29 23:23:00 +0300

    Add initial building description formatter

--- backend/src/services/formatBuildingDescription.ts
+++ backend/src/services/formatBuildingDescription.ts
@@ -1,9 +1,7 @@
 import type { BuildingData } from "../types/building.types.js";
 
-export function formatBuildingDescription(
-    data: BuildingData
-): string {
+export function formatBuildingDescription(data: BuildingData): string {
     return `
-Сграда с идентификатор ${data.identifier}, находяща се в ${data.address}, построена в поземлен имот с идентификатор ${data.landIdentifier}, със застроена площ ${data.builtUpArea}, на ${data.floors} етажа, с предназначение ${data.purpose}.
+Сграда с идентификатор ${data.identifier}, находяща се в ${data.address}, построена в поземлен имот с идентификатор ${data.landIdentifier}, със застроена площ ${data.builtUpArea}, брой етажи: ${data.floors}, с предназначение: ${data.purpose}, при предходен идентификатор: ${data.oldIdentifier}, номер по предходен план: ${data.previousPlanNumberForBuilding}.
     `.trim();
 }
